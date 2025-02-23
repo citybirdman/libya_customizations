c62741b611ddcb0c726c00a8b2ce84e030c10dab
Ahmed Sharaf <ahmed.sharafeddin@gmail.com>
2025-02-23 11:54:13 +0200

    Update purchase_receipt_management.py

--- libya_customizations/libya_customizations/doctype/purchase_receipt_management/purchase_receipt_management.py
+++ libya_customizations/libya_customizations/doctype/purchase_receipt_management/purchase_receipt_management.py
@@ -233,7 +233,7 @@ def get_purchase_receipt_data(purchase_receipt):
 				SELECT
 					name,
 					posting_date,
-					(freight_amount * freight_exchange_rate + inspection_amount * inspection_exchange_rate + clearence_amount + transport_amount + foreign_bank_charges_amount * foreign_bank_charges_exchange_rate + local_bank_charges_amount) / base_grand_total AS landed_cost_prorata       
+					(freight_amount * freight_exchange_rate + inspection_amount * inspection_exchange_rate + clearance_amount + transport_amount + foreign_bank_charges_amount * foreign_bank_charges_exchange_rate + local_bank_charges_amount) / base_grand_total AS landed_cost_prorata       
 				FROM
 					`tabPurchase Receipt`
 				WHERE
