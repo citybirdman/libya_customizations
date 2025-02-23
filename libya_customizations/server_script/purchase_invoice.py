af1694553d8277ecc1fb2f95740f0adbd86823d0
Ahmed Sharaf <ahmed.sharafeddin@gmail.com>
2025-02-23 12:04:01 +0200

    Update purchase_invoice.py

--- libya_customizations/server_script/purchase_invoice.py
+++ libya_customizations/server_script/purchase_invoice.py
@@ -11,8 +11,8 @@ def before_update_after_submit(doc, method):
 			inspection_account_currency = doc.inspection_account_currency,
 			inspection_amount = doc.inspection_amount,
 			inspection_exchange_rate = doc.inspection_exchange_rate,
-			clearence_account = doc.clearence_account,
-			clearence_amount = doc.clearence_amount,
+			clearance_account = doc.clearance_account,
+			clearance_amount = doc.clearance_amount,
 			transport_account = doc.transport_account,
 			transport_amount = doc.transport_amount,
 			foreign_bank_charges_account = doc.foreign_bank_charges_account,
@@ -24,3 +24,13 @@ def before_update_after_submit(doc, method):
 		))
         pr = frappe.get_doc("Purchase Receipt", purchase_receipt)
         pr.save()
+
+def validate_post_carriage_costs(doc, method):
+	if not doc.clearance_account:
+		frappe.throw(_("Clearance Account is mandatory"))
+	if not doc.clearance_amount:
+		frappe.throw(_("Clearance Amount is mandatory"))
+	if not doc.transport_account:
+		frappe.throw(_("Transport Account is mandatory"))
+	if not doc.transport_amount:
+		frappe.throw(_("Transport Amount is mandatory"))
