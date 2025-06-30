209fb4150e6d0c5ef8a1bee5f21d4f215520e4e5
Ahmed Zaytoon <citybirdman@gmail.com>
2025-06-30 14:23:58 +0200

    Making Pricing to be based on Production Year in Sales Invoice

--- libya_customizations/public/sales_invoice.js
+++ libya_customizations/public/sales_invoice.js
@@ -86,3 +86,24 @@ frappe.ui.form.on("Sales Invoice", {
         }
     }
 });
+
+
+frappe.ui.form.on('Sales Invoice Item', {
+    production_year(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+
+        if (frm.doc.is_return === 1 && row.item_code && row.production_year) {
+            frappe.db.get_value('Item Price', {
+                item_code: row.item_code,
+                production_year: row.production_year,
+                selling: 1
+            }, 'price_list_rate').then(r => {
+                if (r && r.message && r.message.price_list_rate) {
+                    frappe.model.set_value(cdt, cdn, 'rate', r.message.price_list_rate);
+                } else {
+                    frappe.msgprint(__('No selling price found for this item and production year.'));
+                }
+            });
+        }
+    }
+});
