04299c52ddad2218164e766b3049c9d3cd9adab4
citybirdman <your-email@domain.com>
2025-12-18 13:51:27 +0000

    fix: payment error when outstanding amount is less than invoice grand total

--- libya_customizations/hooks.py
+++ libya_customizations/hooks.py
@@ -235,7 +235,10 @@ doc_events = {
     "Purchase Receipt": {
         "on_submit": "libya_customizations.server_script.purchase_receipt.on_submit",
          "on_update_after_submit": "libya_customizations.server_script.purchase_receipt.on_update_after_submit"
-    }
+    },
+    # "Stock Ledger Entry": {
+    #     "on_update": "libya_customizations.server_script.stock_ledger_entry.update_item_price"
+    # }
 }
 
 # Scheduled Tasks
---
+++ libya_customizations/server_script/stock_ledger_entry.py
@@ -0,0 +1,54 @@
+import frappe
+
+def update_item_price(doc, method=None):
+
+    values = get_valuation_rate_and_qty(doc.item_code, doc.production_year)
+
+    frappe.db.sql(
+        """
+        UPDATE `tabItem Price`
+        SET stock_valuation_rate = %(stock_valuation_rate)s,
+            stock_qty = %(stock_qty)s,
+            available_qty = %(available_qty)s
+        WHERE item_code = %(item_code)s
+          AND production_year = %(production_year)s
+        """, values,
+    )
+
+
+def get_valuation_rate_and_qty(item_code, production_year):
+
+    avg_rate = frappe.db.sql("""
+        SELECT SUM(stock_value) / SUM(actual_qty)
+        FROM `tabBin`
+        WHERE item_code = %s AND actual_qty > 0
+    """, (item_code,))[0][0]
+
+    # Get Stock Qty (based on item_code + production_year)
+    stock_qty = frappe.db.sql("""
+        SELECT IFNULL(SUM(actual_qty), 0)
+        FROM `tabStock Ledger Entry`
+        WHERE is_cancelled = 0 AND item_code = %s AND IFNULL(production_year, '') = IFNULL(%s, '')
+    """, (item_code, production_year))[0][0]
+
+    qty_to_deliver = frappe.db.sql("""
+        WITH reserved_qty AS (
+            SELECT so.name, IF(SUM(soi.qty - soi.delivered_qty) > 0, SUM(soi.qty - soi.delivered_qty), 0) AS qty_to_deliver
+            FROM `tabSales Order Item` soi
+            INNER JOIN `tabSales Order` so ON soi.parent = so.name
+            WHERE soi.docstatus = 1 AND so.docstatus = 1 AND so.status NOT IN ('Completed', 'Closed') AND soi.qty - soi.delivered_qty > 0
+            AND soi.item_code = %s AND IFNULL(soi.production_year, '') = IFNULL(%s, '')
+        )
+        SELECT IFNULL(SUM(qty_to_deliver), 0)
+        FROM reserved_qty
+    """, (item_code, production_year))[0][0]
+
+    available_qty = stock_qty - qty_to_deliver
+
+    return {
+        "stock_valuation_rate": avg_rate or 0,
+        "stock_qty": stock_qty or 0,
+        "available_qty": available_qty or 0,
+        "item_code": item_code,
+        "production_year": production_year
+    }
