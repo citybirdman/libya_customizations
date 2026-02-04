8b4d10ff4fafc8c6f2e00b1afea2d86d3aa367f0
Ahmed Zaytoon <citybirdman@gmail.com>
2026-02-04 12:10:48 +0000

    Fix: item price doesn't update after repost

--- libya_customizations/__init__.py
+++ libya_customizations/__init__.py
@@ -1,7 +1,18 @@
-import frappe
-from libya_customizations.utils import make_xlsx
+# import frappe
+# from libya_customizations.utils import make_xlsx
 
 __version__ = "0.0.1"
 
-def override_xlsxutils():
-    frappe.utils.xlsxutils.make_xlsx = make_xlsx
+# def override_xlsxutils():
+#     frappe.utils.xlsxutils.make_xlsx = make_xlsx
+
+try:
+    import frappe
+    import erpnext
+    from erpnext.stock.stock_ledger import update_entries_after
+    from libya_customizations.overrides.repost_sl import process_sle
+
+    update_entries_after.process_sle = process_sle
+    print("Override applied: update_entries_after.process_sle")
+except Exception as e:
+    print(f"Error applying override: {e}")
